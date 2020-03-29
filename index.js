e46b981ee5a88aa0862057d556a3525914ce0b63
Andreia Pena <andreia.pena.ferreira@gmail.com>
2020-03-29 21:47:12 +0200

    ex04

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ console.log ("Connected");
 } else{
     console.log ("Not Authorized");
 };
+
+//ex04 
+let a = 8;
+ let b = a+1;
+a +=1 ;
+a *= 2;
+b -= 5;
+b *= b;
+a += 3;
+
+if (a==1){
+   console.log (a +=2);
+} else {
+   console.log (a +=3)
+};
+console.log("result :", a);
