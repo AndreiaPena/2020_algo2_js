645110ccdd31d85b27fd61b55cf164c4fe3250f2
Andreia Pena <andreia.pena.ferreira@gmail.com>
2020-03-29 21:46:47 +0200

    ex03

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 //ex02
 var unNombre = 9;
 console.log(typeof unNombre);
+
+//ex03
+let isLogin = true;
+if (isLogin==true){
+console.log ("Connected");
+} else{
+    console.log ("Not Authorized");
+};
